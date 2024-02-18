146dfe480e6db6baee7b66040242bf2989f4bac7
fatimzehraa <elbouaazzaouitema@gmail.com>
2024-02-18 19:36:11 +0100

    not complete

--- ex04/Program.java
+++ ex04/Program.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.SortedMap;
 /*
 AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAASSSSSSSSSSSSSSSSSSSSSSSSDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDWEWWKFKKDKKD
 SKAKLSLDKSKALLLLLLLLLLRTRTETWTWWWWWWWWWWOOOOOOO42
 */
-public class Main {
+public class Program {
     static int occurrence(String str, char c){
         int count = 0;
         for (int i = 0; i < str.length(); i++){
@@ -14,20 +15,29 @@ public class Main {
         }
         return count;
     }
+    static int[] mostFrequent(String str, ArrayList<Integer> occurrences){
+        ,
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // String input = scanner.nextLine();
+        String input = scanner.nextLine();
+        System.out.println(input);
         String chars = "";
         ArrayList<Integer> occurrences = new ArrayList<Integer>();
+        SortedMap<Integer, Character> sortedOccurrences = new SortedMap<Integer, Character>();
         char c;
+        int i = 0;
         while (true) {
-            c = scanner.next(".").charAt(0);
-            if (chars.indexOf(c) != -1)//exists
-                continue;
-            chars = chars + c;
-            occurrences.add(occurrence(chars, c));
-            if (scanner.next().equals("42"))
+            c = input.charAt(i);
+            System.out.println(c);
+            if (c == '4' && input.charAt(i + 1) == '2')
                 break;
+            if (chars.indexOf(c) == -1){//exists
+                chars = chars + c;
+                sortedOccurrences.put(occurrence(input, c), c);
+                occurrences.add(occurrence(input, c));
+            }
+            i++;
         }
         System.out.println(occurrences);
     }
